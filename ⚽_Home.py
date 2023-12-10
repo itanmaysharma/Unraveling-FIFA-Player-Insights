fc5ea36838bdced407861f9ee950d931143b935b
Tanmay Sharma <tanmay.rsh@gmail.com>
2023-12-09 20:45:00 -0500

    finalised code.

--- ⚽_Home.py
+++ ⚽_Home.py
@@ -7,28 +7,30 @@ st.set_page_config(
 )
 
 
-st.write("# Welcome to FootForesight! 👋")
-
+st.write("# Welcome to FootForesight👋")
 
 st.markdown(
     '''
-    FootForesight is a Web Application which leverages the results 
-    from the project " From Virtual Pitch to Data: Unraveling FIFA Player Insights".
-    
-    The main objective for the project mentioned was to extract valuable insights from FIFA21 dataset
-    by taking advantage of ML models and answers below questions -
-    1. What are the most significant attributes associated with a player and how their overall 
-       rating is impacted based on these attributes, and can we build a predictive model for 
-       this? 
-    2. What are the attributes affecting a player’s market value and what are the trends in it?
-    3. What are other possible hidden trends in the dataset that can help in improving 
-       gameplay balance leading to enhanced playing experience?
-    
-    FootForesight is a user-friendly interface which lets you give new data and generate desirable outcomes.
-    
-    There are currently three main features which can be accessed from sidebar.
-    
-    Relevant instructions are mentioned on each page.
+    :white[FootForesight is a Web Application which leverages the results
+    from the project " From Virtual Pitch to Data: Unraveling FIFA Player Insights".]
+
+    :white[The main objective for the project mentioned was to extract valuable insights from FIFA21 dataset
+    by taking advantage of ML models and answers below questions -]
+
+    :white[1. What are the most significant attributes associated with a player and how their overall
+       rating is impacted based on these attributes, and can we build a predictive model for
+       this?]
+
+    :white[2. What are the attributes affecting a player’s market value and what are the trends in it?]
+
+    :white[3. What are other possible hidden trends in the dataset that can help in improving
+       gameplay balance leading to enhanced playing experience?]
+
+    :white[FootForesight is a user-friendly interface which lets you give new data and generate desirable outcomes.]
+
+    :white[There are currently three main features which can be accessed from sidebar.]
+
+    :white[Relevant instructions are mentioned on each page.]
 '''
 )
 
@@ -53,4 +55,6 @@ def set_bg(main_bg):
         unsafe_allow_html=True
     )
 
+
 set_bg('backgrounds/bg4.jpg')
+
